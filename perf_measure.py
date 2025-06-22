3db47e5c811d536fb034215b5510937752c9a987
Peter Chettiar <89821181+peterchettiar@users.noreply.github.com>
2025-06-22 14:13:34 +0000

    UPD: Performance Metrics

--- perf_measure.py
+++ perf_measure.py
@@ -1,6 +1,6 @@
 """
 The performance metrics class evaluates the performance of strategies that was
-implemented in the TradingVolatility class. 
+implemented in the TradingVolatility class.
 
 The class computes the Key Performance Indicators (KPIs) for each strategy,
 such as the Geometric Return, Sharpe Ratio, Calmer Ration and other relevant metrics.
@@ -16,62 +16,58 @@ class PerformanceMetrics:
     the volatility trading strategies.
     """
 
-    def __init__(
-        self, df: pd.DataFrame, portfolio_value_col_name: str, index_col_name: str
-    ):
-        self.df = df
-        self.portfolio_value_col_name = portfolio_value_col_name
-        self.index_col_name = index_col_name
-
-    def _year_start_helper(self, series) -> list[str]:
-
-        tmp = series.copy().reset_index()
-        res = (
-            tmp.groupby(tmp[self.index_col_name].dt.year)[self.index_col_name]
-            .min()
-            .tolist()
-        )
-
-        return res[1:]
-
-    def _year_end_helper(self, series) -> list[str]:
-
-        tmp = series.copy().reset_index()
-        res = (
-            tmp.groupby(tmp[self.index_col_name].dt.year)[self.index_col_name]
-            .max()
-            .tolist()
-        )
-
-        return res[1:-1]
-
-    def average_annulised_geometric_return(self) -> float:
+    def average_annual_geometric_return(self, portfolio_value: pd.Series) -> float:
         """
         Non-risk adjusted return - For GeRs, it will be considered between
         beginning of each year portfolio value and end of year value
         """
 
-        tmp = self.df[self.portfolio_value_col_name].copy()
-        year_start = self._year_start_helper(tmp)
-        year_end = self._year_end_helper(tmp)
+        tmp = portfolio_value.copy()
 
-        tmp = tmp[tmp.index.isin(year_start) | tmp.index.isin(year_end)][:-1].to_frame()
+        year_start = tmp.groupby(tmp.index.year).agg(lambda x: x.index.min()).tolist()
+        year_end = tmp.groupby(tmp.index.year).agg(lambda x: x.index.max()).tolist()
 
-        start_pv = []
-        end_pv = []
+        res = pd.DataFrame(
+            data=[
+                {
+                    "YEAR_START": s_date,
+                    "PV_START": tmp.loc[s_date],
+                    "YEAR_END": e_date,
+                    "PV_END": tmp.loc[e_date],
+                }
+                for s_date, e_date in zip(year_start, year_end)
+            ]
+        )
 
-        for date in tmp.index:
-            if date.month == 1:
-                start_pv.append(tmp.loc[date][0])
-            else:
-                end_pv.append(tmp.loc[date][0])
-
-        res = pd.DataFrame(index=range(2012, 2020))
-        res[["start_pv", "end_pv"]] = pd.Series([start_pv, end_pv])
         res["log_return"] = res.apply(
-            lambda x: np.log(x["end_pv"] / x["start_pv"]), axis=1
+            lambda x: np.log(x["PV_END"] / x["PV_START"]), axis=1
         )
 
         ger = res["log_return"].sum() / len(res)
 
         return ger
+
+    def annualised_downside_vol(
+        self, portfolio_return: pd.Series, mar: float = 0
+    ) -> float:
+        """
+        Measuring the volatility of negative returns (i.e. returns that are below
+        a minimum acceptable return - in our case zero), on an annualised basis.
+        """
+
+        tmp = portfolio_return.copy()
+
+        annual_dow_vol = np.sqrt(
+            tmp[tmp < mar].map(lambda x: x**2).sum() / len(tmp)
+        ) * np.sqrt(252)
+
+        return annual_dow_vol
+
+    def annualised_vol(self, portfolio_return: pd.Series) -> float:
+        """
+        Measuring the annualised total standard deviation of portfolio returns.
+        """
+
+        tmp = portfolio_return.copy()
+
+        return tmp.std() * np.sqrt(252)
